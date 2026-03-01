dfbbce5e318044770a35375bd2a1db207e1cd5fa
glithcat <khleborodov21@gmail.com>
2026-03-01 12:38:36 +0000

    feature: added models for e-diary

--- e_diary/models.py
+++ e_diary/models.py
@@ -1,3 +1,20 @@
 from django.db import models
-
 # Create your models here.
+class Diary(models.Model):
+    """
+        модель щоденника 
+         
+         -через grades прописуеться звязок між таблицями оцінок та таблицею щоденника
+    """
+    user_profile = models.OneToOneField()
+    grades = models.ForeignKey(Grade,on_delete=models.CASCADE)
+
+
+class Grade(models.Model):
+    """
+        Модель для привязки запису оцінки до користувача
+    """
+    user_profile = models.OneToOneField(on_delete=models.CASCADE)#TODO:поміняти назву якось
+    grade = models.SmallIntegerField()
+
+
