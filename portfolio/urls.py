968efd78b1dd32ca6aca59e2a555b17d9b48c686
p1xarid <atras.oleg@gmail.com>
2026-03-15 13:49:41 +0200

    fix

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -2,11 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("portfolio/<str:username>/", views.portfolio_view, name="portfolio"),
-    path("portfolio/", views.my_portfolio_view, name="my_portfolio"),
+    path("<str:username>/", views.portfolio_view, name="portfolio"),
+    path("", views.my_portfolio_view, name="my_portfolio"),
     path("settings/", views.settings_view, name="settings"),
     path("account-settings/", views.account_settings_view, name="account_settings"),
-    path("register/", views.register_view, name="register"),
-    path("login/", views.login_view, name="login"),
-    path("logout/", views.logout_view, name="logout"),
 ]
