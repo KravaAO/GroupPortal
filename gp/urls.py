e02dbe5eb6d72ccf5fbeab4f0d8faaf7286e072f
langernikoletta <langernikoletta2020@gmail.com>
2026-03-15 13:23:18 +0200

    fix merge conflict

--- gp/urls.py
+++ gp/urls.py
@@ -1,11 +1,12 @@
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.templatetags.static import static
+from django.urls import include, path
+from django.views.generic.base import RedirectView
 
 urlpatterns = [
+    path('favicon.ico', RedirectView.as_view(url=static('main/favicon.png'), permanent=True)),
+    path('', include('main.urls')),
     path('admin/', admin.site.urls),
+    path('forum/', include('forum.urls')),
 ]
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
