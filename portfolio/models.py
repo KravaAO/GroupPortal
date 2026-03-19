f5926bd614e462bef24f557972883d7b3d9740c0
p1xarid <atras.oleg@gmail.com>
2026-03-19 21:35:26 +0200

    fix

--- portfolio/models.py
+++ portfolio/models.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-    avatar = models.ImageField(upload_to="pictures/", blank=True, null=True)
+    avatar = models.ImageField(upload_to="static/pictures/", blank=True, null=True)
     bio = models.TextField(max_length=200, blank=True, null=True)
 
     @property
